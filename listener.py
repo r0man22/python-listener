762143c2efc136538a19efaed703a8812221509b
r0man22 <roman.mammadov22@gmail.com>
2025-01-11 21:20:59 -0500

    nc

--- listener.py
+++ listener.py
@@ -13,3 +13,19 @@ def execute(cmd):
         return
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
+
+
+class NetCat:
+    def __init__(self,args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+
