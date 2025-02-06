8f92ff9b81e1325c18cdd522df186df83ff98fee
r0man22 <roman.mammadov22@gmail.com>
2025-02-06 14:36:19 -0500

    proxy

--- proxy.py
+++ proxy.py
@@ -2,31 +2,40 @@ import sys
 import socket
 import threading
 
-# Yazdırılabilir karakterleri tanımlayan filtre
 HEX_FILTER = ''.join(
     [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)]
 )
 
 def hexdump(src, length=16, show=True):
-    # Eğer veri byte türündeyse, string'e dönüştür
     if isinstance(src, bytes):
         src = src.decode()
 
     results = []
 
-    # Veriyi 'length' uzunluğunda parçalara ayırarak işleme
     for i in range(0, len(src), length):
-        word = str(src[i:i+length])  # Parçalanmış veri
-        printable = word.translate(HEX_FILTER)  # Yazdırılabilir karakterler
-        hexa = ' '.join([f'{ord(c):02X}' for c in word])  # Hexadecimal karşılıkları
+        word = str(src[i:i+length]) 
+        printable = word.translate(HEX_FILTER)  
+        hexa = ' '.join([f'{ord(c):02X}' for c in word]) 
 
-        # Hexadecimal değerlerin genişliği
         hexwidth = length * 3
         results.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
 
-    # Sonuçları ekrana yazdır veya döndür
     if show:
         for line in results:
             print(line)
     else:
         return results
+
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
