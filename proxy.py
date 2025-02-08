85c057bf95f0a12ffa5e9bd0135f55df1a80f746
r0man22 <roman.mammadov22@gmail.com>
2025-02-08 14:15:04 -0500

    proxy

--- proxy.py
+++ proxy.py
@@ -89,3 +89,24 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print("[*] No more data. Closing connections.")
             break
 
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print('Problem on bind: %r' % e)
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+            sys.exit(0)
+
+    print("[*] Listening on %s:%d" % (local_host, local_port))
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+        line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+        print(line)
+
+        proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port, receive_first))
+        proxy_thread.start()
